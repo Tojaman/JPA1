f0ee6120be2595346a0c8af4be69b46698a0a4cd
CHOHYUNGJUNE <89668546+Tojaman@users.noreply.github.com>
2023-11-05 18:24:36 +0900

    완성

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -28,12 +28,29 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
 
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); // 주문 수량만큼 재고에서 빼기
+        return orderItem;
+    }
+
     //==비즈니스 로직==//
 //    주문 취소시 재고 수량 원복
     public void cancel() {
         getItem().addStock(count);
     }
 
+    //==조회 로직==//
+
+
+    /**
+     * 주문 상품 전체 가격 조회
+     */
     public int getTotalPrice() {
         return getOrderPrice() * getCount();
     }
